747310236d9b93ef83eea832a1578b287d8c26e0
denis <diachkov.da@gmail.com>
2023-05-02 20:36:53 +0200

    Wandb

--- codes/train.py
+++ codes/train.py
@@ -5,23 +5,48 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as sched
 import utils
+import pytorch_lightning as pl
 from dataset.datamodule import DataModule
 from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import TensorBoardLogger as tb
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.utilities import rank_zero_only
+
+
+# Crutch for PyTorch Lightning to stop logging useless metrics
+class _Wandblogger(WandbLogger):
+    @rank_zero_only
+    def log_metrics(self, metrics, step=None):
+        metrics.pop('epoch', None)
+        metrics.pop('global_step', None)
+        super().log_metrics(metrics=metrics, step=step)
+
+
+class LogCodeAndConfigCallback(pl.Callback):
+    def __init__(self, cfg) -> None:
+        super().__init__()
+        self.cfg = cfg
+
+    @rank_zero_only
+    def on_fit_start(self, trainer, pl_module):
+        trainer.logger.experiment.log_code(
+            root=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        )
+        trainer.logger.experiment.config.update({**self.cfg})
+
 
 
 def train(cfg: dict):
     if not os.path.exists(cfg.experiment_path):
         os.makedirs(cfg.experiment_path)
-    tb_logger = tb(".", "experiments", version=cfg.experiment_name)
+    logger = _Wandblogger(**cfg.logger_params)
     callbacks = [
         utils.get_obj(callback.callback)(
             **callback.callback_params | ({"dirpath": cfg.experiment_path} if callback.callback.endswith("ModelCheckpoint") else {})
         ) 
         for callback in cfg.trainer_callbacks
-    ]
+    ] + [LogCodeAndConfigCallback(cfg)]
     trainer = Trainer(
-        logger=tb_logger,
+        logger=logger,
         callbacks=callbacks,
         **cfg.trainer_params
     )
